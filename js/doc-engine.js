96e15edaaec4307f08e0eaefd6ebf47c5dcbad1f
essentialols <47343314+essentialols@users.noreply.github.com>
2026-09-25 00:08:27 -0700

    Default EPUB table columns to tabs

--- js/doc-engine.js
+++ js/doc-engine.js
@@ -239,7 +239,7 @@ function resolveEpubPath(opfPath, href) {
 
 /** Parse EPUB (ZIP) and extract chapter text in spine order. */
 async function extractEpubText(file, onProgress, options = {}) {
-  const cellSeparator = options.cellSeparator ?? '\t';
+  options.cellSeparator = options.cellSeparator ?? '\t';
   if (onProgress) onProgress(10);
   const buf = new Uint8Array(await file.arrayBuffer());
   if (typeof fflate === 'undefined') throw new Error('ZIP library not loaded. Please reload the page.');
